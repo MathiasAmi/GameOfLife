8defc191ab1ad658cdb69ed57e9f1cf2fc76a5ab
MathiasAmi <math35i0@edu.easj.dk>
2019-04-24 15:50:10 +0200

    Added timeline and addrandom cell to the game

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,41 +1,48 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class Controller {
 
-    @FXML
-    Button btn_start;
+    Game game = new Game();
+    Timeline timeline;
 
-    @FXML
-    Button btn_stop;
-
+    
     @FXML
     GridPane gp_board;
-    Game game = new Game();
 
     public void fillGrid(ActionEvent actionEvent) {
 
-        game.update();
-
-        for (int i = 0; i < Game.BOARDSIZEX; i++) {
-            for (int j = 0; j < Game.BOARDSIZEY; j++) {
-                if(game.getBoard()[i][j].isAlive()){
-                    gp_board.add(new Rectangle(24,18, Color.valueOf("RED")), i,j);
-                } else {
-                    gp_board.add(new Rectangle(24,18, Color.valueOf("BLACK")), i,j);
+        timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            for (int i = 0; i < Game.BOARDSIZEX; i++) {
+                for (int j = 0; j < Game.BOARDSIZEY; j++) {
+                    if (game.getBoard()[i][j].isAlive()) {
+                        gp_board.add(new Rectangle(24, 18, Color.valueOf("YELLOW")), i, j);
+                    } else {
+                        gp_board.add(new Rectangle(24, 18, Color.valueOf("GREY")), i, j);
+                    }
                 }
             }
-        }
+            game.update();
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
-
+        ///////// Stops the animation when pressed /////////
     public void stop(ActionEvent actionEvent) {
-        game.addLivingCell();
+        timeline.stop();
 
+    }
+       ///////// adds random cells and starts the animation /////////
+    public void inputRandom(ActionEvent actionEvent) {
+        game.addLivingCell();
     }
 }
